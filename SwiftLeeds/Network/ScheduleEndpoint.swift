13c4e4af96aa4f33338d27dfd89b1e7910bfd476
Alex Logan <me@alexanderlogan.co.uk>
2022-08-08 19:42:05 +0100

    Pull out ScheduleEndpoint and Environment into their own files

--- SwiftLeeds/Network/ScheduleEndpoint.swift
+++ SwiftLeeds/Network/ScheduleEndpoint.swift
@@ -7,11 +7,6 @@
 
 import NetworkKit
 
-// MARK: - Environment
-struct SwiftLeedsEnvironment: NetworkEnvironmentProviding {
-    let apiURL: String = "https://www.swiftleeds.co.uk/api/v1"
-}
-
 // MARK: - Schedule Endpoint
 struct ScheduleEndpoint: Endpoint {
     typealias DataType = Schedule
