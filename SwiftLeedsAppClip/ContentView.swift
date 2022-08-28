43dd201f704a1996b469b2692b50c4c91e8119ce
Muralidharan Kathiresan <kmuralidharan91@gmail.com>
2022-08-28 22:36:09 +0100

    Adds App Clip Configuration to the Project

---
+++ SwiftLeedsAppClip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftLeedsAppClip
+//
+//  Created by Muralidharan Kathiresan on 25/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftLeedsAppClip/SwiftLeedsAppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLeedsAppClipApp.swift
+//  SwiftLeedsAppClip
+//
+//  Created by Muralidharan Kathiresan on 25/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLeedsAppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
