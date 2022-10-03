72bef7bfe22347e8608897b030c726ef9324e1de
Muralidharan Kathiresan <kmuralidharan91@gmail.com>
2022-10-03 11:58:44 +0100

    Adds MyConferenceView to the AppClip

--- SwiftLeedsAppClip/SwiftLeedsAppClipApp.swift
+++ SwiftLeedsAppClip/SwiftLeedsAppClipApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import NetworkKit
 
 @main
 struct SwiftLeedsAppClipApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MyConferenceView()
+                .environment(\.network, network)
         }
     }
 }
