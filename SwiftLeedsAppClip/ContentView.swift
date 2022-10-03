ada3da8c65519215d0482c6924bd48ccdf51cf87
Muralidharan Kathiresan <kmuralidharan91@gmail.com>
2022-10-03 11:44:12 +0100

    Add AppClip to the Project

--- SwiftLeedsAppClip/ContentView.swift
+++ SwiftLeedsAppClip/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world App Clip!")
         }
         .padding()
     }
