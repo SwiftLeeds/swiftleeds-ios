396f551bff0da948969d6727dcafc4c5c15c509a
Muralidharan Kathiresan <kmuralidharan91@gmail.com>
2022-10-03 11:32:46 +0100

    Adds App Clip Configuration to the Project

---
+++ SwiftLeedsAppClip/SwiftLeedsAppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLeedsAppClipApp.swift
+//  SwiftLeedsAppClip
+//
+//  Created by Muralidharan Kathiresan on 03/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLeedsAppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
