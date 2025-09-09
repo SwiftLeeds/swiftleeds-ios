fd583b53a73402ce111f5bca0dcdb1ee4cedf6d2
Muralidharan Kathiresan <kmuralidharan91@gmail.com>
2025-09-09 16:39:30 +0100

    Adds Social Links to the About Page

--- SwiftLeeds/Views/About/AboutView.swift
+++ SwiftLeeds/Views/About/AboutView.swift
@@ -14,6 +14,8 @@ struct AboutView: View {
     private let venueURL = URL(string: "https://swiftleeds.co.uk/#venue")
     private let codeOfConductURL = URL(string: "https://swiftleeds.co.uk/conduct")
     private let reportAProblemLink = "https://forms.gle/PJie9aRNAtzQUdUu9"
+    private let slackURL = URL(string: "https://join.slack.com/t/swiftleedsworkspace/shared_invite/zt-3dex3vb3k-JNYQ~ollX6R619D_tZVfXQ")
+    private let youtubeURL = URL(string: "https://www.youtube.com/@swiftleeds")
 
     var body: some View {
         SwiftLeedsContainer {
@@ -34,16 +36,24 @@ struct AboutView: View {
             VStack(spacing: Padding.cellGap) {
                 StackedTileView(primaryText: "About", secondaryText: Strings.aboutSwiftLeeds)
 
-                CommonTileButton(primaryText: "Report a problem", accessibilityHint: "Opens a web view to allow a problem to be reported", backgroundStyle: Color.cellBackground) {
+                CommonTileButton(icon: "exclamationmark.triangle.fill", primaryText: "Report a problem", accessibilityHint: "Opens a web view to allow a problem to be reported", backgroundStyle: Color.cellBackground) {
                     isReportAProblemShown = true
                 }
 
-                CommonTileButton(primaryText: "Code of conduct", accessibilityHint: "Opens a web page showing our code of conduct", backgroundStyle: Color.cellBackground) {
+                CommonTileButton(icon: "doc.text.fill", primaryText: "Code of conduct", accessibilityHint: "Opens a web page showing our code of conduct", backgroundStyle: Color.cellBackground) {
                     openURL(url: codeOfConductURL)
                 }
                 
-                CommonTileButton(primaryText: "Venue", accessibilityHint: "Opens a web page showing our venue information", backgroundStyle: Color.cellBackground) {
+                CommonTileButton(icon: "location.fill", primaryText: "Venue", accessibilityHint: "Opens a web page showing our venue information", backgroundStyle: Color.cellBackground) {
                     openURL(url: venueURL)
+                }
+                
+                CommonTileButton(icon: "message.fill", primaryText: "Join our Slack", accessibilityHint: "Opens an invite link to join the SwiftLeeds Slack workspace", backgroundStyle: Color.cellBackground) {
+                    openURL(url: slackURL)
+                }
+                
+                CommonTileButton(icon: "play.rectangle.fill", primaryText: "YouTube Channel", accessibilityHint: "Opens the SwiftLeeds YouTube channel", backgroundStyle: Color.cellBackground) {
+                    openURL(url: youtubeURL)
                 }
             }
             .fitToReadableContentGuide(type: .width)
