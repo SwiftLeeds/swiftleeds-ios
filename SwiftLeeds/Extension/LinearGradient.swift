1d3b0b5e15baae449b91e0473ff7e4eea57cab97
Paul Willis <ios_dev@paulrobertwillis.com>
2025-11-22 16:16:32 +0000

    chore(LinearGradient): move extension to folder called from
    Prepares for move into some shared view module

--- SwiftLeeds/Extension/LinearGradient.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import SwiftUI
-
-extension LinearGradient {
-    static let weather = LinearGradient(colors: [
-        .weatherGradientStart, .weatherGradientEnd
-    ], startPoint: .bottomLeading, endPoint: .topLeading)
-}
---
+++ SwiftLeeds/Views/Components/Extensions/LinearGradient+Extensions.swift
@@ -0,0 +1,13 @@
+import Foundation
+import SwiftUI
+
+extension LinearGradient {
+    static let weather = LinearGradient(
+        colors: [
+            .weatherGradientStart,
+            .weatherGradientEnd
+        ],
+        startPoint: .bottomLeading,
+        endPoint: .topLeading
+    )
+}
