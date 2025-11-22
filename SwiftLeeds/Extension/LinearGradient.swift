0e0edce6d2c5f7068be4c6e6a8d5518a6b82a8ba
Paul Willis <ios_dev@paulrobertwillis.com>
2025-11-22 16:14:04 +0000

    chore(LinearGradient): remove unused `announcement` property

--- SwiftLeeds/Extension/LinearGradient.swift
+++ SwiftLeeds/Extension/LinearGradient.swift
@@ -5,8 +5,4 @@ extension LinearGradient {
     static let weather = LinearGradient(colors: [
         .weatherGradientStart, .weatherGradientEnd
     ], startPoint: .bottomLeading, endPoint: .topLeading)
-
-    static let announcement = LinearGradient(colors: [
-        .buyTicketGradientStart, .buyTicketGradientEnd
-    ], startPoint: .bottomLeading, endPoint: .topLeading)
 }
