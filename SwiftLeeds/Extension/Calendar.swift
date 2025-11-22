0d1480400931d74e1aa56052a7ce7bed2da11451
Paul Willis <ios_dev@paulrobertwillis.com>
2025-11-22 16:10:04 +0000

    refactor(Calendar): move `Calendar.numberOfDays(to:)` to an extension in caller file
    Only used once so moving to call site to simplify code

--- SwiftLeeds/Extension/Calendar.swift
+++ SwiftLeeds/Extension/Calendar.swift
@@ -1,14 +1,6 @@
 import Foundation
 
 extension Calendar {
-    func numberOfDays(to date: Date) -> Int {
-        let fromDate = startOfDay(for: Date.now)
-        let toDate = startOfDay(for: date)
-        let numberOfDays = dateComponents([.day], from: fromDate, to: toDate)
-
-        return numberOfDays.day ?? 0
-    }
-    
     static var atConferenceVenue: Calendar {
         var calendar = Calendar.current
         calendar.timeZone = .init(abbreviation: "BST")!
