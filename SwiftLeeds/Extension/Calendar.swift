70584b6157a036aa1c70ac4297abf705f3eb9918
Paul Willis <ios_dev@paulrobertwillis.com>
2025-11-22 16:11:26 +0000

    refactor(Calendar): move `Calendar.atConferenceVenue` to an extension in caller file
    Only used once so moving to call site to simplify code

--- SwiftLeeds/Extension/Calendar.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension Calendar {
-    static var atConferenceVenue: Calendar {
-        var calendar = Calendar.current
-        calendar.timeZone = .init(abbreviation: "BST")!
-        return calendar
-    }
-}
--- SwiftLeeds/Extension/Date.swift
+++ SwiftLeeds/Extension/Date.swift
@@ -9,3 +9,11 @@ extension Date {
         return date
     }
 }
+
+private extension Calendar {
+    static var atConferenceVenue: Calendar {
+        var calendar = Calendar.current
+        calendar.timeZone = .init(abbreviation: "BST")!
+        return calendar
+    }
+}
