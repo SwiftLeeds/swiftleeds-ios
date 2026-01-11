142d006e22f958ff0d108464d2bdc4f7a72e4700
Paul Willis <ios_dev@paulrobertwillis.com>
2026-01-11 21:36:18 +0000

    style(String.noEmojis): prefer `!` for negating boolean over comparison with `==`

--- SwiftLeeds/Extension/String.swift
+++ SwiftLeeds/Extension/String.swift
@@ -3,7 +3,7 @@ import Foundation
 extension String {
     var noEmojis: String {
         return self.unicodeScalars
-            .filter { $0.properties.isEmojiPresentation == false }
+            .filter { !$0.properties.isEmojiPresentation }
             .reduce("") { $0 + String($1) }
     }
 }
