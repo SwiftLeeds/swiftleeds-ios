564e75ca9b8d422edef3a6fb898e92caee0bb298
Paul Willis <paul.willis@tyneside.tech>
2026-08-08 18:39:26 +0100

    feat(AuthenticationFeature): add TicketReference value type

---
+++ Packages/AuthenticationFeature/Sources/AuthenticationFeature/Domain/TicketReference.swift
@@ -0,0 +1,27 @@
+public struct TicketReference: Hashable, Sendable {
+    public enum ParsingError: Error, Equatable {
+        case invalidFormat
+    }
+
+    private let storage: String
+
+    /// Creates a ticket reference from the given string.
+    /// - Parameter value: The ticket reference.
+    /// - Throws: ``ParsingError/invalidFormat`` if `value` is not a valid ticket reference.
+    public init(_ value: String) throws(ParsingError) {
+        guard value.wholeMatch(of: /[A-Z0-9]{4}-[0-9]/) != nil else {
+            throw .invalidFormat
+        }
+        self.storage = value
+    }
+
+    fileprivate var stringValue: String { storage }
+}
+
+extension String {
+    /// Creates a string from a ticket reference.
+    /// - Parameter ticketReference: The ticket reference to convert.
+    public init(_ ticketReference: TicketReference) {
+        self = ticketReference.stringValue
+    }
+}
