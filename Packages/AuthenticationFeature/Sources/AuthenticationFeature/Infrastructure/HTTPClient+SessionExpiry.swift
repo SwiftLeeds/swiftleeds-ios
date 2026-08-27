b96153de12d614b33da13c121e51ce341dd3aa73
Paul Willis <paul.willis@tyneside.tech>
2026-08-27 13:44:04 +0100

    refactor(AuthenticationFeature): detect session expiry with HTTPStatus

--- Packages/AuthenticationFeature/Sources/AuthenticationFeature/Infrastructure/HTTPClient+SessionExpiry.swift
+++ Packages/AuthenticationFeature/Sources/AuthenticationFeature/Infrastructure/HTTPClient+SessionExpiry.swift
@@ -4,7 +4,7 @@ extension HTTPClient {
     package func interceptingSessionExpiry(onExpiry: @escaping @Sendable () async -> Void) -> HTTPClient {
         HTTPClient { request in
             let (data, response) = try await self.send(request)
-            if response.statusCode == 401, request.value(forHTTPHeaderField: "Authorization") != nil {
+            if response.status == .unauthorized, request.value(forHTTPHeaderField: "Authorization") != nil {
                 await onExpiry()
             }
             return (data, response)
