5a2ac8ad040b0c4cd9c3c38288823926943b5c5d
Paul Willis <paul.willis@tyneside.tech>
2026-09-13 11:28:04 +0100

    refactor(ScheduleFeature): mark the schedule types Sendable
    They are structs of value types, and a stored schedule crosses an isolation
    boundary.

--- Packages/ScheduleFeature/Sources/ScheduleFeature/Domain/Schedule.swift
+++ Packages/ScheduleFeature/Sources/ScheduleFeature/Domain/Schedule.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-public struct Schedule: Codable {
+public struct Schedule: Codable, Sendable {
     public let data: Data
 
     public init(data: Data) {
         self.data = data
     }
 
-    public struct Data: Codable {
+    public struct Data: Codable, Sendable {
         public enum ParsingError: Error, Equatable {
             case noDays
         }
@@ -35,7 +35,7 @@ public struct Schedule: Codable {
         }
     }
 
-    public struct Day: Codable, Identifiable {
+    public struct Day: Codable, Identifiable, Sendable {
         public let date: Foundation.Date
         public let name: String
         public let slots: [Slot]
@@ -51,7 +51,7 @@ public struct Schedule: Codable {
         }
     }
 
-    public struct Event: Codable, Identifiable {
+    public struct Event: Codable, Identifiable, Sendable {
         public let id: UUID
         public let name: String
         public let location: String
@@ -69,7 +69,7 @@ public struct Schedule: Codable {
         }
     }
 
-    public struct Slot: Identifiable {
+    public struct Slot: Identifiable, Sendable {
         public let id: UUID
         public let date: Foundation.Date?
         public let startTime: String
