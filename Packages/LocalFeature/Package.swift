a5e35dbd49166969c956de0b52f29cc8961fa28a
Paul Willis <paul.willis@tyneside.tech>
2026-09-17 12:28:58 +0100

    feat(LocalFeature): add Coordinate, a latitude and a longitude
    First type of the package that the Local screen's data moves into.

---
+++ Packages/LocalFeature/Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "LocalFeature",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v13),
+    ],
+    products: [
+        .library(name: "LocalFeature", targets: ["LocalFeature"]),
+    ],
+    dependencies: [
+        .package(path: "../NetworkKit"),
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "LocalFeature",
+            dependencies: [
+                .product(name: "Dependencies", package: "swift-dependencies"),
+                .product(name: "NetworkKit", package: "NetworkKit"),
+            ]
+        ),
+        .testTarget(
+            name: "LocalFeatureTests",
+            dependencies: [
+                "LocalFeature",
+                .product(name: "Dependencies", package: "swift-dependencies"),
+                .product(name: "NetworkKit", package: "NetworkKit"),
+            ]
+        ),
+    ]
+)
---
+++ Packages/LocalFeature/Tests/LocalFeatureTests/Domain/CoordinateTests.swift
@@ -0,0 +1,12 @@
+import LocalFeature
+import Testing
+
+@Suite struct CoordinateTests {
+    @Test(arguments: [(-90.0, -180.0), (90.0, 180.0), (53.797378, -1.545209)])
+    func whenBothValuesAreOnTheGlobe_shouldKeepThem(latitude: Double, longitude: Double) throws {
+        let sut = try Coordinate(latitude: latitude, longitude: longitude)
+
+        #expect(sut.latitude == latitude)
+        #expect(sut.longitude == longitude)
+    }
+}
